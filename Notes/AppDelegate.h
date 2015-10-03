b46410cc53f11f0a4fba7ff582505a88795aaf6e
Cameron Spickert <cspickert@users.noreply.github.com>
2015-10-02 20:33:24 -0400

    Initial Commit

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by Cameron Spickert on 10/2/15.
+//  Copyright (c) 2015 Cameron Spickert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notes
+//
+//  Created by Cameron Spickert on 10/2/15.
+//  Copyright (c) 2015 Cameron Spickert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
